e84ebb80c48195489f94407298e7186fa2601bbd
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-08-26 09:51:43 +0600

    feat(CONFIG-SERVER): add config server for common configurations

---
+++ ConfigServer/ConfigServer/src/test/java/com/classmate/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classmate.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
